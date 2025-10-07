fd639c5b6685c5b4d37a0436097c0f236158546c
Tachera Sasi <tacherasasi@gmail.com>
2025-10-07 12:33:22 +0300

    Add SSE handler and update HTML to receive messages

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main(){
@@ -21,4 +22,13 @@ func sseHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
+	
+	for i := range 10 {
+		fmt.Fprintf(w, "data: Message %d\n\n", i)
+		fmt.Println("Sent message", i)
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
